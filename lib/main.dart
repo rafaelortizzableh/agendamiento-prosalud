b7e458a2344ee8555822155cc27afd57002bced7
Rafael Ortiz <sumercedrafa@gmail.com>
2021-08-18 19:18:09 +0200

    +font-assets, subButton

--- lib/main.dart
+++ lib/main.dart
@@ -37,43 +37,7 @@ class Form extends StatelessWidget {
       body: LayoutBuilder(
         builder: (context, constraints) {
           if (constraints.maxWidth < 600) {
-            return ListView(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-              children: [
-                NameWidget(),
-                SizedBox(height: 16),
-                TypeOfIdDropdown(),
-                IdNumberWidget(),
-                SizedBox(height: 16),
-                AgeWidget(),
-                SizedBox(height: 16),
-                DiagnosisWidget(),
-                SizedBox(height: 16),
-                AddressWidget(),
-                SizedBox(height: 16),
-                InsuranceWidget(),
-                SizedBox(height: 16),
-                TelephoneWidget(),
-                SizedBox(height: 16),
-                DepartmentDropdown(),
-                LocalityDropdown(),
-                SizedBox(height: 16),
-                SessionsWidget(),
-                SizedBox(height: 16),
-                AuthorizationDate(),
-                SizedBox(height: 16),
-                PreferedScheduleDropdown(),
-                SizedBox(height: 16),
-                ElevatedButton(
-                  onPressed: () {},
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20.0, vertical: 15.0),
-                    child: Text('Solicitar cita'),
-                  ),
-                ),
-              ],
-            );
+            return OneColumnLayout();
           } else {
             return Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -109,6 +73,46 @@ class Form extends StatelessWidget {
           }
         },
       ),
+    );
+  }
+}
+
+class OneColumnLayout extends StatelessWidget {
+  const OneColumnLayout({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+      children: [
+        NameWidget(),
+        SizedBox(height: 16),
+        TypeOfIdDropdown(),
+        IdNumberWidget(),
+        SizedBox(height: 16),
+        AgeWidget(),
+        SizedBox(height: 16),
+        DiagnosisWidget(),
+        SizedBox(height: 16),
+        AddressWidget(),
+        SizedBox(height: 16),
+        InsuranceWidget(),
+        SizedBox(height: 16),
+        TelephoneWidget(),
+        SizedBox(height: 16),
+        DepartmentDropdown(),
+        LocalityDropdown(),
+        SizedBox(height: 16),
+        SessionsWidget(),
+        SizedBox(height: 16),
+        AuthorizationDate(),
+        SizedBox(height: 16),
+        PreferedScheduleDropdown(),
+        SizedBox(height: 16),
+        SubmitButton(),
+      ],
     );
   }
 }
