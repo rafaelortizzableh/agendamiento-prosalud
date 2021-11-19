8adc9f06b3d34f42ad2bfec1399098de5d7f27fd
Rafael Ortiz <sumercedrafa@gmail.com>
2021-11-19 11:31:51 +0100

    Replace ListView with SliverList

--- lib/screens/form/layouts/one_column_layout.dart
+++ lib/screens/form/layouts/one_column_layout.dart
@@ -5,34 +5,53 @@ class OneColumnLayout extends StatelessWidget {
   const OneColumnLayout({
     Key? key,
   }) : super(key: key);
+  static const _padding12 = 12.0;
+  static const _padding0 = 0.0;
+  static const _zeroValue = 0;
+  static const _oneValue = 1;
+  static const _nineValue = 9;
+  static const _fourteenValue = 14;
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      itemCount: _oneColumnWidgets.length,
-      itemBuilder: (context, index) => _oneColumnWidgets[index],
-      separatorBuilder: (context, index) =>
-          SizedBox(height: (index == 1 || index == 9) ? 0 : 16),
-      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-      shrinkWrap: true,
+    return CustomScrollView(
+      slivers: [
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) => Padding(
+                padding: EdgeInsets.fromLTRB(
+                  _padding12,
+                  (index == _zeroValue || index == _fourteenValue)
+                      ? _padding12
+                      : _padding0,
+                  _padding12,
+                  (index == _oneValue || index == _nineValue)
+                      ? _padding0
+                      : _padding12,
+                ),
+                child: _oneColumnWidgets[index]),
+            childCount: _oneColumnWidgets.length,
+          ),
+        ),
+      ],
     );
   }
 }
 
-List<Widget> _oneColumnWidgets = [
-  const NameWidget(),
-  const TypeOfIdDropdown(),
-  const IdNumberWidget(isTwoColumn: false),
-  const AgeWidget(),
-  const DiagnosisWidget(),
-  const TypeOfTherapyDropDown(),
-  const AddressWidget(),
-  const InsuranceWidget(),
-  const TelephoneWidget(),
-  const DepartmentDropdown(),
-  const LocalityDropdown(isTwoColumn: false),
-  const SessionsWidget(),
-  const AuthorizationDate(),
-  const PreferredScheduleDropdown(),
-  const SubmitButton(),
+const List<Widget> _oneColumnWidgets = const [
+  NameWidget(),
+  TypeOfIdDropdown(),
+  IdNumberWidget(isTwoColumn: false),
+  AgeWidget(),
+  DiagnosisWidget(),
+  TypeOfTherapyDropDown(),
+  AddressWidget(),
+  InsuranceWidget(),
+  TelephoneWidget(),
+  DepartmentDropdown(),
+  LocalityDropdown(isTwoColumn: false),
+  SessionsWidget(),
+  AuthorizationDate(),
+  PreferredScheduleDropdown(),
+  SubmitButton(),
 ];
