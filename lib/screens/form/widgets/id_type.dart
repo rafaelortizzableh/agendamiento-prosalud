fedb235559758f8b582d46f109ef6f9cfe606357
Rafael Ortiz <sumercedrafa@gmail.com>
2022-03-09 10:09:23 +0100

    Replaced list with const types

--- lib/screens/form/widgets/id_type.dart
+++ lib/screens/form/widgets/id_type.dart
@@ -1,26 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+
 import '../../../providers/providers.dart';
 import '../../../models/models.dart';
 import '../../../core/core.dart';
 
 class TypeOfIdDropdown extends StatelessWidget {
   const TypeOfIdDropdown({Key? key}) : super(key: key);
+  static const List<TypeOfId> _typeOfIds = [
+    TypeOfId(type: 'Cédula de ciudadanía', id: 1),
+    TypeOfId(type: 'Tarjeta de identidad', id: 2),
+    TypeOfId(type: 'Registro Civil', id: 2),
+    TypeOfId(type: 'Cédula de extranjería', id: 4),
+    TypeOfId(type: 'Pasaporte', id: 5),
+  ];
+  static final List<String> _idNames = _typeOfIds.map((e) => e.type).toList();
+
   @override
   Widget build(BuildContext context) {
     return Consumer(
       builder: (context, watch, _) {
-        List<TypeOfId> _typeOfIds = [
-          TypeOfId(type: 'Cédula de ciudadanía', id: 1),
-          TypeOfId(type: 'Tarjeta de identidad', id: 2),
-          TypeOfId(type: 'Registro Civil', id: 2),
-          TypeOfId(type: 'Cédula de extranjería', id: 4),
-          TypeOfId(type: 'Pasaporte', id: 5),
-        ];
-        List<String> idNames = _typeOfIds.map((e) => e.type).toList();
         return AppDropdownInput<String?>(
           hintText: "Tipo de documento*",
-          options: idNames,
+          options: _idNames,
           value: watch(typeOfIdProvider).state?.type,
           onChanged: (String? value) async {
             context.read(typeOfIdProvider).state =
