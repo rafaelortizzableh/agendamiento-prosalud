a548511e026ef612d765e05e6a6dbb4d897b0818
Rafael Ortiz <sumercedrafa@gmail.com>
2022-05-12 01:11:56 +0200

    - Added MacOS Compatibility

---
+++ build_and_deploy.sh
@@ -0,0 +1,3 @@
+#/bin/bash!
+flutter build web --dart-define=SHEETS_API_URL=https://en1lc94w82bp4f1.m.pipedream.net --dart-define=EMAIL_API_URL=https://enzf2gdm7c0p9io.m.pipedream.net
+firebase deploy
